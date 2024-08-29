bcfb6b52a2c5685bfa075d1a1c132bcb96bfbfad
lseongmi <w2307@e-mirim.hs.kr>
2024-08-29 23:08:59 +0900

    kotlin first time

---
+++ app/src/main/java/com/example/a20240829/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.a20240829
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button2)
+        button.setOnClickListener{
+            Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left,systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
