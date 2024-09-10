3678c09afd94792dfb81750b25eda26a5287cf47
lseongmi <w2307@e-mirim.hs.kr>
2024-09-10 19:57:55 +0900

    Toast 사용

--- app/src/main/java/com/example/a20240829/MainActivity.kt
+++ app/src/main/java/com/example/a20240829/MainActivity.kt
@@ -10,18 +10,17 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var button: Button
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.test_main)
+        setContentView(R.layout.main_0910)
 
-        button = findViewById(R.id.button2)
-        button.setOnClickListener{
-            Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
+        var btn1 : Button = findViewById(R.id.button1)
+        btn1.setOnClickListener {
+            Toast.makeText(applicationContext, "버튼클릭", Toast.LENGTH_LONG).show()
         }
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
